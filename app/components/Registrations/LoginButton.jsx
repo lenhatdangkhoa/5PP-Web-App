58410641547a1c3b2b03d2e4741ccfcf18a2b404
Khoa Le <kl58277@uga.edu>
2023-10-20 14:41:16 -0400

    modify files

--- app/components/Registrations/LoginButton.jsx
+++
@@ -1,6 +0,0 @@
-"use client"
-import React from "react"
-
-export default function LoginButton() {
-    return <button className="btn btn-secondary" onClick={() => console.log("Logging In")}>Log In</button>
-}
--- app/components/Registrations/RegistrationLayout.jsx
+++ app/components/Registrations/RegistrationLayout.jsx
@@ -1,11 +1,49 @@
-import React from "react"
-import LoginButton from "./LoginButton"
-import RegisterButton from "./RegisterButton"
+"use client"
+import React, {useState, useEffect} from "react"
 
 export default function RegistrationLayout() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    async function handleSubmit(event) {
+        event.preventDefault(); // prevent default page refreshing
+
+        /*
+        * Sending a post request to the server to with the username and password
+        */
+        const response = await fetch("http://localhost:3001/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ "username" : username , "password" : password}),
+        });
+
+        if (response.ok) {
+            console.log("Received response from server");
+        } else {
+            console.error("Error while sending POST request");
+        }
+    }
+
     return <div>
-        
-        <LoginButton />
-        <RegisterButton />
-    </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+            <input type="text" className="border-2 border-indigo-600" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"></input>
+            </div>
+            <div>
+            <input type="text" className="border-2 border-indigo-600" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"></input>
+            </div>
+            <div>
+            <input type="submit" className="btn btn-secondary"/>
+            </div>
+        </form>
+        </div>
+    
 }
