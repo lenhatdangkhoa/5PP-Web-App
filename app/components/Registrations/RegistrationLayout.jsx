3703340274d562f3f0e8e5bbaa3e5a2d6e34038d
Khoa Le <kl58277@uga.edu>
2023-10-25 14:34:23 -0400

    fix password constraints

--- app/components/Registrations/RegistrationLayout.jsx
+++ app/components/Registrations/RegistrationLayout.jsx
@@ -6,18 +6,27 @@ export default function RegistrationLayout() {
     const [password, setPassword] = useState("")
     const [validPassword, setValidPassword] = useState(true);
 
+    const is8Char = password.length >= 8
+    const isUpper = /[A-Z]/.test(password)
+    const isLower = /[a-z]/.test(password)
+    const hasNum = /[0-9]/.test(password)
+    const hasSpecial = /[!@#$%^&*]/.test(password)
+
+
+
     function handlePasswordChange(e) {
         const pass = e.target.value;
         setPassword(pass);
         setValidPassword(isPasswordValid(pass));
       }
-    const isPasswordValid = (value) => {
+    function isPasswordValid(value) {
+        if (value.length == 0) return true
         return (
-          value.length >= 8
-        //   /[A-Z]/.test(value) &&
-        //   /[a-z]/.test(value) &&
-        //   /[0-9]/.test(value) &&
-        //   /[!@#$%^&*]/.test(value)
+          value.length >= 8 && 
+           /[A-Z]/.test(value) &&
+          /[a-z]/.test(value) &&
+          /[0-9]/.test(value) &&
+          /[!@#$%^&*]/.test(value)
         );
       };
     async function handleSubmit(event) {
@@ -40,32 +49,32 @@ export default function RegistrationLayout() {
         }
     }
 
-    return <div>
+    return <div className="flex justify-center bg-slate-200">
         <form onSubmit={handleSubmit}>
             <div>
             <input type="text" className="border-2 border-indigo-600" 
                 value={username} 
-                onChange={handlePasswordChange}
+                onChange={(e) => setUsername(e.target.value)}
                 placeholder="Username"></input>
             </div>
             <div>
             <input type="password" className="border-2 border-indigo-600" 
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={handlePasswordChange}
                 placeholder="Password"></input>
             </div>
             <div>
             <input type="submit" className="btn btn-secondary" value="Register"/>
             </div>
         </form>
-        {validPassword && (<div className="bg-red-100 border border-red-500 text-red-900 px-4 py-3 rounded relative" role="alert">
+        {!validPassword && (<div className="bg-slate-300 border border-slate-600 text-slate-800 px-4 py-3 rounded relative" role="alert">
             <strong className="font-bold">Password requirements:</strong>
             <ul className="list-disc pl-5 mt-2">
-                <li>Your password must be at least 8 characters long.</li>
-                <li>Include at least one uppercase letter.</li>
-                <li>Include at least one lowercase letter.</li>
-                <li>Include at least one number.</li>
-                <li>Include at least one special character (e.g., @, #, $, %).</li>
+                <li className={is8Char ? "text-green-600" : ""}>Your password must be at least 8 characters long.</li>
+                <li className={isUpper ? "text-green-600" : ""}>Include at least one uppercase letter.</li>
+                <li className={isLower ? "text-green-600" : ""}>Include at least one lowercase letter.</li>
+                <li className={hasNum ? "text-green-600" : ""}>Include at least one number.</li>
+                <li className={hasSpecial ? "text-green-600" : ""}>Include at least one special character (e.g., @, #, $, %).</li>
             </ul>
         </div>)}
         
