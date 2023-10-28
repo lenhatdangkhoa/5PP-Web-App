2cb38aa0b0f6fb8aed8ae842cb32036d4b0d440a
Khoa Le <kl58277@uga.edu>
2023-10-27 22:12:45 -0400

    working on main page

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello, World</h1>
+      <a href="/registration"><button>Sign Up</button></a>
     </main>
   )
 }
