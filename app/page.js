53b49a8fac0038e20d10e5b3375d3dcd83c85afb
Khoa Le <kl58277@uga.edu>
2023-11-03 16:40:02 -0400

    working on main page

--- app/page.js
+++ app/page.js
@@ -4,8 +4,10 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello, World</h1>
+      <div className="bg-sky-500/50 flex justify-center flex-col">
       <Link href="/registration"><button className="btn " >Sign Up</button></Link>
+      <Link href="/signin"><button className="btn " >Sign In</button></Link>
+      </div>
     </main>
   )
 }
