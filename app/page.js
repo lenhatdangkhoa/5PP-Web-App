a9c7d636741baa4cc0605d8c74dfa9163f4b4939
Khoa Le <kl58277@uga.edu>
2023-11-06 16:23:54 -0500

    fixing button on main page

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className="bg-sky-500/50 flex justify-center flex-col">
-      <Link href="/registration"><button className="btn " >Sign Up</button></Link>
-      <Link href="/signin"><button className="btn " >Sign In</button></Link>
+      <div className="flex flex-col justify-center">
+        <Link href="/registration" className>Sign Up</Link>
+        <Link href="/signin"><button className="btn" >Sign In</button></Link>
       </div>
     </main>
   )
